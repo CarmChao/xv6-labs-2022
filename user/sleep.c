3a8ce8aae22144345899580f9b2a1edb9748018d
chao <724183484zz@gmail.com>
2022-09-12 11:28:04 +0800

    lab: Add Lab Utilities - sleep

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    write(1, "Usage: sleep n\n", 15);
+    exit(1);
+  }
+
+  int sleep_time = atoi(argv[1]);
+  sleep(sleep_time);
+  exit(0);
+}
