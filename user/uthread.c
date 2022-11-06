f1c081272cbb7b00838cda088e191454d1599f2e
chao <724183484zz@gmail.com>
2022-11-06 19:25:11 +0800

    lab: Add Lab Multithreading

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,37 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
+  void (*func)();
+  int has_start;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
-void 
+
+void
 thread_init(void)
 {
   // main() is thread 0, which will make the first invocation to
@@ -62,6 +83,12 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    if (current_thread->has_start == 0) {
+      current_thread->has_start = 1;
+      current_thread->context.ra = (uint64)current_thread->func;
+      current_thread->context.sp = (uint64)(current_thread->stack + STACK_SIZE);
+    }
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,6 +103,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->func = func;
+  t->has_start = 0;
 }
 
 void 
